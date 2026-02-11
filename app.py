578279d4535ffce9c3d309c899a540678b59e2ab
pshyduck <ambrus.david13@gmail.com>
2026-02-11 15:44:11 +0100

    új kepek

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ with app.app_context():
 
 # ---- STATIKUS ADATOK (Szobák) ----
 rooms_data = [
-    {"id": 1, "name": "Kétágyas szoba", "description": "Kényelmes szoba 2 fő részére.", "image": "images/room1.jpg"},
-    {"id": 2, "name": "Családi szoba", "description": "Tágas szoba 4 főnek.", "image": "images/room2.jpg"},
+    {"id": 1, "name": "Kétágyas szoba", "description": "Kényelmes szoba 2 fő részére.", "image": "images/francia1.jpg"},
+    {"id": 2, "name": "Családi szoba", "description": "Tágas szoba 4 főnek.", "image": "images/ketagyas2.jpg"},
 ]
 
 # ---- SEGÉDFÜGGVÉNY A SZABAD HELYEKHEZ ----
